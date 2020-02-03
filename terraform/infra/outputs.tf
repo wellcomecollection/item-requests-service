30960ab0546883864e28efb00d190fa850b8c7e1
Alex Chan <a.chan@wellcome.ac.uk>
2020-02-03 18:34:39 +0000

    fix some more bits

--- terraform/infra/outputs.tf
+++ terraform/infra/outputs.tf
@@ -7,11 +7,19 @@ output "vpc_id" {
 }
 
 output "cognito_user_pool_arn" {
-  value = data.terraform_remote_state.infra_critical.outputs.cognito_user_pool_arn
+  # value = data.terraform_remote_state.infra_critical.outputs.cognito_user_pool_arn
+  # the cognito stuff is in TF0.12
+  # TODO: Update this to TF0.12
+  # TODO: The value in the remote state doesn't match the value below
+  value = "arn:aws:cognito-idp:eu-west-1:760097843905:userpool/eu-west-1_oToO0mWFj"
 }
 
 output "cognito_api_identifier" {
-  value = data.terraform_remote_state.infra_critical.outputs.cognito_stacks_api_identifier
+  # value = data.terraform_remote_state.infra_critical.outputs.cognito_stacks_api_identifier
+  # the cognito stuff is in TF0.12
+  # TODO: Update this to TF0.12
+  # TODO: The value in the remote state doesn't match the value below
+  value = "eu-west-1_oToO0mWFj"
 }
 
 output "static_content_bucket_name" {
