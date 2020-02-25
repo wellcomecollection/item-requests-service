b5b5457329427b14398b63081df050067bf6e686
Robert Kenny <r.kenny@wellcome.ac.uk>
2020-02-25 10:49:23 +0000

    add sierra base url config

--- terraform/stack/services.tf
+++ terraform/stack/services.tf
@@ -10,7 +10,8 @@ module "items_api" {
     app_base_url       = "https://api.wellcomecollection.org/stacks/v1/items"
     context_url        = "https://api.wellcomecollection.org/stacks/v1/context.json"
     catalogue_base_url = "https://api.wellcomecollection.org/catalogue/v2"
-
+    sierra_base_url    = "https://libsys.wellcomelibrary.org/iii/sierra-api"
+    
     metrics_namespace = "items_api"
   }
 
@@ -42,6 +43,7 @@ module "requests_api" {
     app_base_url       = "https://api.wellcomecollection.org/stacks/v1/requests"
     context_url        = "https://api.wellcomecollection.org/stacks/v1/context.json"
     catalogue_base_url = "https://api.wellcomecollection.org/catalogue/v2"
+    sierra_base_url    = "https://libsys.wellcomelibrary.org/iii/sierra-api"
 
     metrics_namespace = "requests_api"
   }
