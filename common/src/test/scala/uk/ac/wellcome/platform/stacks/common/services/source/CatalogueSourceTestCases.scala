cb5454bf68f6d9714b1e070f927276c44636812a
Alex Chan <alex@alexwlchan.net>
2020-02-27 13:50:04 +0000

    Add a test for searching the catalogue API

--- common/src/test/scala/uk/ac/wellcome/platform/stacks/common/services/source/CatalogueSourceTestCases.scala
+++ common/src/test/scala/uk/ac/wellcome/platform/stacks/common/services/source/CatalogueSourceTestCases.scala
@@ -3,8 +3,8 @@ package uk.ac.wellcome.platform.stacks.common.services.source
 import org.scalatest.{FunSpec, Matchers}
 import org.scalatest.concurrent.ScalaFutures
 import uk.ac.wellcome.fixtures.TestWith
-import uk.ac.wellcome.platform.stacks.common.models.StacksWorkIdentifier
-import uk.ac.wellcome.platform.stacks.common.services.source.CatalogueSource.{IdentifiersStub, ItemStub, TypeStub, WorkStub}
+import uk.ac.wellcome.platform.stacks.common.models.{SierraItemIdentifier, StacksWorkIdentifier}
+import uk.ac.wellcome.platform.stacks.common.services.source.CatalogueSource._
 
 trait CatalogueSourceTestCases[CatalogueSourceImpl <: CatalogueSource] extends FunSpec with Matchers with ScalaFutures {
   def withCatalogueSource[R](testWith: TestWith[CatalogueSourceImpl, R]): R
@@ -61,6 +61,62 @@ trait CatalogueSourceTestCases[CatalogueSourceImpl <: CatalogueSource] extends F
         )
 
         whenReady(future) { _ shouldBe expectedWork }
+      }
+    }
+
+    it("can search for an identifier") {
+      withCatalogueSource { catalogueSource =>
+        val future = catalogueSource.getSearchStub(
+          identifier = SierraItemIdentifier(1093837)
+        )
+
+        val expectedSearch = SearchStub(
+          totalResults = 2,
+          results = List(
+            WorkStub(
+              id = "aczz5tm3",
+              items = List(
+                ItemStub(
+                  id = Some("qkdy3h58"),
+                  identifiers = Some(
+                    List(
+                      IdentifiersStub(
+                        identifierType = TypeStub(id = "sierra-system-number", label = "Sierra system number"),
+                        value = "i11088953"
+                      ),
+                      IdentifiersStub(
+                        identifierType = TypeStub(id = "sierra-identifier", label = "Sierra identifier"),
+                        value = "1108895"
+                      )
+                    )
+                  )
+                )
+              )
+            ),
+            WorkStub(
+              id = "ayzrznsz",
+              items = List(
+                ItemStub(
+                  id = Some("q2knsrhh"),
+                  identifiers = Some(
+                    List(
+                      IdentifiersStub(
+                        identifierType = TypeStub(id = "sierra-system-number", label = "Sierra system number"),
+                        value = "i10938370"
+                      ),
+                      IdentifiersStub(
+                        identifierType = TypeStub(id = "sierra-identifier", label = "Sierra identifier"),
+                        value = "1093837"
+                      )
+                    )
+                  )
+                )
+              )
+            )
+          )
+        )
+
+        whenReady(future) { _ shouldBe expectedSearch }
       }
     }
   }
