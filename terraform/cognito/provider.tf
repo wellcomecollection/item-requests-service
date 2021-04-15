ca1d9f12607ac3a71af93eef800e558c87f90cde
Alex Chan <a.chan@wellcome.org>
2021-04-15 15:04:10 +0100

    Bin a provider I don't actually need

--- terraform/cognito/provider.tf
+++ terraform/cognito/provider.tf
@@ -5,21 +5,3 @@ provider "aws" {
     role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
   }
 }
-
-# This provider is being kept around to clean up the associated ACM cert
-# for id.wellcomecollection.org.
-#
-# If there's no longer an `id.wellcomecollection.org` certificate in
-# us-east-1 in the catalogue account, you can delete this provider.
-#
-# ACM won't let you delete a certificate that it thinks is in use – and it's
-# taking a while to realise we've removed this certificate from a
-# Cognito/CloudFront distribution.
-provider "aws" {
-  region = "us-east-1"
-  alias  = "us_east_1"
-
-  assume_role {
-    role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
-  }
-}
