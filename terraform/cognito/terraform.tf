75842fac0cee33730948da69ba81403d30b84ea4
Alex Chan <a.chan@wellcome.org>
2021-04-15 14:09:03 +0100

    Add the Cognito Terraform from the catalogue repo

---
+++ terraform/cognito/terraform.tf
@@ -0,0 +1,20 @@
+terraform {
+  required_version = ">= 0.12"
+
+  backend "s3" {
+    role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
+
+    bucket         = "wellcomecollection-catalogue-infra-delta"
+    key            = "terraform/stacks-service/cognito.tfstate"
+    dynamodb_table = "terraform-locktable"
+    region         = "eu-west-1"
+  }
+}
+
+provider "aws" {
+  region = "eu-west-1"
+
+  assume_role {
+    role_arn = "arn:aws:iam::756629837203:role/catalogue-developer"
+  }
+}
