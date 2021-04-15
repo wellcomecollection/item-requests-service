8598cb39a3c96d8a6b2b8372037de864618121d7
Alex Chan <a.chan@wellcome.org>
2021-04-15 09:04:07 +0100

    Remove an unused Terraform provider

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,7 +28,6 @@ module "prod_stack" {
   static_content_bucket_name = module.infra.static_content_bucket_name
 
   providers = {
-    aws.platform = aws.platform
-    aws.dns      = aws.dns
+    aws.dns = aws.dns
   }
 }
--- terraform/provider.tf
+++ terraform/provider.tf
@@ -18,16 +18,6 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias = "platform"
-
-  region = var.aws_region
-
-  assume_role {
-    role_arn = "arn:aws:iam::760097843905:role/platform-developer"
-  }
-}
-
-provider "aws" {
   alias = "dns"
 
   region = "eu-west-1"
